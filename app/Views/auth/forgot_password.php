7a32516425a99f7ef0dd5780fdde00b846898423
Silva-Adams~ <sandilegushana@gmail.com>
2026-05-02 20:50:57 +0200

    Adding styles to the pages and fixing the the errors on the varriables

--- app/Views/auth/forgot_password.php
+++ app/Views/auth/forgot_password.php
@@ -1,15 +1,27 @@
-<h1><?php echo lang('Auth.forgot_password_heading');?></h1>
-<p><?php echo sprintf(lang('Auth.forgot_password_subheading'), $identity_label);?></p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="<?php echo base_url('styles/style.css'); ?>">
+      <title>Document</title>
+</head>
+<body>
+      <h1><?php echo lang('Auth.forgot_password_heading');?></h1>
+       <p><?php echo sprintf(lang('Auth.forgot_password_subheading'), $identity_label ?? '');?></p>
 
-<div id="infoMessage"><?php echo $message;?></div>
+            <div id="infoMessage"><?php echo $message ?? '';?></div>
 
-<?php echo form_open('auth/forgot_password');?>
+            <?php echo form_open('auth/forgot_password');?>
 
-      <p>
-      	<label for="identity"><?php echo (($type === 'email') ? sprintf(lang('Auth.forgot_password_email_label'), $identity_label) : sprintf(lang('Auth.forgot_password_identity_label'), $identity_label));?></label> <br />
-      	<?php echo form_input($identity);?>
-      </p>
+            <p>
+      	      <label for="identity"><?php echo (($type ?? '' === 'email') ? sprintf(lang('Auth.forgot_password_email_label'), $identity_label ?? '') : sprintf(lang('Auth.forgot_password_identity_label'), $identity_label ?? ''));?></label> <br />
+      	      <?php echo form_input($identity ?? []);?>
+            </p>
 
       <p><?php echo form_submit('submit', lang('Auth.forgot_password_submit_btn'));?></p>
 
-<?php echo form_close();?>
+      <?php echo form_close();?>
+      
+</body>
+</html>
--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,36 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo base_url('styles/style.css'); ?>">
+  <title>Login</title>
+</head>
+  <body>
 
-<?php
+    <h1><?php echo lang('Auth.login_heading');?></h1>
+    <p><?php echo lang('Auth.login_subheading');?></p>
 
-use Config\IonAuth; 
-?>
+    <div id="infoMessage"><?php echo $message ?? '';?></div>
 
-<h1><?php echo lang('Auth.login_heading');?></h1>
-<p><?php echo lang('Auth.login_subheading');?></p>
+    <?php echo form_open('auth/login');?>
 
-<div id="infoMessage"><?php echo $message ?? '';?></div>
+      <p>
+        <?php echo form_label(lang('Auth.login_identity_label'), 'identity');?>
+        <?php echo form_input($identity ?? []);?>
+      </p>
 
-<?php echo form_open('auth/login');?>
+      <p>
+        <?php echo form_label(lang('Auth.login_password_label'), 'password');?>
+        <?php echo form_input($password ?? "");?>
+      </p>
 
-  <p>
-    <?php echo form_label(lang('Auth.login_identity_label'), 'identity');?>
-    <?php echo form_input($identity ?? []);?>
-  </p>
-
-  <p>
-    <?php echo form_label(lang('Auth.login_password_label'), 'password');?>
-    <?php echo form_input($password ?? "");?>
-  </p>
-
-  <p>
-    <?php echo form_label(lang('Auth.login_remember_label'), 'remember');?>
-    <?php echo form_checkbox('remember', '1', false, 'id="remember"');?>
-  </p>
-
-
-  <p><?php echo form_submit('submit', lang('Auth.login_submit_btn'));?></p>
-
-<?php echo form_close();?>
-
-<p><a href="forgot_password"><?php echo lang('Auth.login_forgot_password');?></a></p>
+      <p>
+        <?php echo form_label(lang('Auth.login_remember_label'), 'remember');?>
+        <?php echo form_checkbox('remember', '1', false, 'id="remember"');?>
+      </p>
 
 
+      <p><?php echo form_submit('submit', lang('Auth.login_submit_btn'));?></p>
+
+    <?php echo form_close();?>
+
+    <p><a href="forgot_password"><?php echo lang('Auth.login_forgot_password');?></a></p>
+  </body>
+</html>
